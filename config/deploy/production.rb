4c601c613fcd33fc86e34d52570a1eb2aca5de2c
marikosaito <marikovalle@icloud.com>
2019-01-22 19:12:29 +0900

    Change ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.178.233.80', user: 'ec2-user', roles: %w{app db web}
+server '52.198.24.179', user: 'ec2-user', roles: %w{app db web}
